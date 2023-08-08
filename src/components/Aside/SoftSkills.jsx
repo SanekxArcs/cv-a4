95ac3f1b46c8416244aced39ec57056456f9b311
SanekxArcs <56607007+SanekxArcs@users.noreply.github.com>
2023-08-08 19:04:04 +0200

    Update SoftSkills.jsx

--- src/components/Aside/SoftSkills.jsx
+++ src/components/Aside/SoftSkills.jsx
@@ -5,14 +5,14 @@ const SoftSkills = () => {
     <>
       <motion.div className="cursor-default select-none break-before-auto">
         <h3>Soft Skills</h3>
-        <ul className="flex flex-col gap-1">
-          <li>Communication</li>
-          <li>Teamwork</li>
-          <li>Problem-solving</li>
-          <li>Adaptability</li>
-          <li>Time management</li>
-          <li>Creativity</li>
-          <li>Leadership skills</li>
+        <ul className="flex flex-col gap-1 text-base list-disc">
+          <li>Effective interaction and exchange of information.</li>
+          <li>Guiding and inspiring a group towards success.</li>
+          <li>Innovative thinking and originality.</li>
+          <li>Efficient allocation and utilization of time.</li>
+          <li>Adjusting in different situations.</li>
+          <li>Collaborative efforts towards a common goal.</li>
+          <li>Finding solutions to challenges and obstacles.</li>
         </ul>
       </motion.div>
     </>
