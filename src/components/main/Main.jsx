a714b49560e947a9fab73f7a928bbc51dccd88bf
SanekxArcs <56607007+SanekxArcs@users.noreply.github.com>
2023-08-20 19:20:14 +0200

    Update Main.jsx

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -16,23 +16,24 @@ const Main = ({ nameRef }) => {
         className="flex flex-col col-span-2 gap-10 px-5"
       >
         <div id="top" className="hidden select-none md:block">
-          <motion.h1>Oleksandr Dzisiak</motion.h1>
-          <motion.h2>Frontend developer</motion.h2>
+          <motion.h1 className="">Oleksandr Dzisiak</motion.h1>
+          <motion.h2 className="">Frontend developer</motion.h2>
           <motion.p ref={nameRef} className="text-lg">
-            I am a highly <b>motivated</b>  professional with a passion for creating
-            visually appealing and user-friendly web applications. My expertise
-            encompasses effective interaction and exchange of information to
-            ensure seamless communication. I excel in collaborative efforts
-            towards a common goal, demonstrating teamwork that guides and
-            inspires a group towards success. <br /> In addition to my proficiency in
+            I am a highly <b>motivated</b> professional with a passion for
+            creating visually appealing and user-friendly web applications. My
+            expertise encompasses effective interaction and exchange of
+            information to ensure seamless communication. I excel in
+            collaborative efforts towards a common goal, demonstrating teamwork
+            that guides and inspires a group towards success. <br /> In addition
+            to my proficiency in
             <b> HTML, CSS, JavaScript </b>, I specialize in finding solutions to
             challenges and obstacles, showcasing my adaptability and
             problem-solving skills. Also i have experience with popular
-            frameworks such as <b>React.js</b> . <br /> Time management is another forte, as I
-            am adept at efficient allocation and utilization of time, ensuring
-            timely delivery of high-quality work. Beyond this, I actively seek
-            to learn and integrate new technologies, reflecting my dedication to
-            continuous improvement.
+            frameworks such as <b>React.js</b> . <br /> Time management is
+            another forte, as I am adept at efficient allocation and utilization
+            of time, ensuring timely delivery of high-quality work. Beyond this,
+            I actively seek to learn and integrate new technologies, reflecting
+            my dedication to continuous improvement.
             <br />
             <a
               href="https://o-d.netlify.app/"
@@ -40,8 +41,8 @@ const Main = ({ nameRef }) => {
               rel="noopener noreferrer"
             >
               <span className="p-2 text-xs transition-all rounded-lg hover:ring-2 ring-emerald-400">
-                <i className="pr-2 fa-solid fa-link "></i>Click here to see my <b>full</b> and 
-                latest resume updates
+                <i className="pr-2 fa-solid fa-link "></i>Click here to see my{" "}
+                <b>full</b> and latest resume updates
               </span>
             </a>
           </motion.p>
